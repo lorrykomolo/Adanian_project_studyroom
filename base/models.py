00d2ff364aff244eecdbe494577e0ee95f3c8268
lorry <lorrykomoloofficial@gmail.com>
2022-06-22 09:00:23 +0300

    navigation and serch bars

--- base/models.py
+++ base/models.py
@@ -20,13 +20,17 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.name
 
 
-    class Message(models.Model):
+class Message(models.Model):
         user = models.ForeignKey(User, on_delete=models.CASCADE)
-        room = models.ForeignKey(Topic, on_delete=models.CASCADE)
+        room = models.ForeignKey(Room, on_delete=models.CASCADE)
         body = models.TextField()
         updated = models.DateTimeField(auto_now=True)
         created = models.DateTimeField(auto_now_add=True)
