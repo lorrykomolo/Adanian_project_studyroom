b09a4274ea0738781f4fd303d59610c19bbd7fe9
lorry <lorrykomoloofficial@gmail.com>
2022-06-28 20:41:52 +0300

    included requirements file and gitignore

--- base/views.py
+++ base/views.py
@@ -72,24 +72,24 @@ def home(request):
     return render(request, 'base/home.html', context)
     
 def room(request, pk):
-        room = Room.objects.get(id=pk)
-        room_messages = room.message_set.all()
-        participants = room.participants.all()
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
 
 
 
 
-        if request.method == 'POST':
-            message = Message.objects.create(
-                user=request.user,
-                room=room,
-                body=request.POST.get('body')
-            )
-            room.participants.add(request.user)
-            return redirect('room', pk=room.id)
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
             
-        context = {'room': room, 'room_messages': room_messages, 'participants': participants}
-        return render(request, 'base/room.html', context)
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
+    return render(request, 'base/room.html', context)
 
 
 def userProfile(request, pk):
