1a4a90d2d24c22a41ccde681b545ebe457faa50c
lorry <lorrykomoloofficial@gmail.com>
2022-06-29 17:47:46 +0300

    heroku deploy

--- studyroom/settings.py
+++ studyroom/settings.py
@@ -160,6 +160,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ALL_ORIGINS = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
