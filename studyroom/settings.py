f065f52fbacde2b318edad52bfb51277abb2de4b
lorry <lorrykomoloofficial@gmail.com>
2022-07-01 23:46:09 +0300

    deploy

--- studyroom/settings.py
+++ studyroom/settings.py
@@ -147,10 +147,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR / 'static')
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'static/images')
 
 
 
